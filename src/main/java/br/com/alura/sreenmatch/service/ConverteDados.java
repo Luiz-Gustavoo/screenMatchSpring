298bd8452c800a50b599a6e0f348d56803c06726
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-15 22:08:36 -0300

    Adicionar classe ConverteDados

---
+++ src/main/java/br/com/alura/sreenmatch/service/ConverteDados.java
@@ -0,0 +1,8 @@
+package br.com.alura.sreenmatch.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+}
