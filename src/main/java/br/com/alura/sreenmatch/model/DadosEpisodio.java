44d4eae54b1cba40a42ef06a8a35c70a50ea5671
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-20 20:21:54 -0300

    Adicionar comentário

--- src/main/java/br/com/alura/sreenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/sreenmatch/model/DadosEpisodio.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosEpisodio(@JsonAlias("Title") String titulo,
+public record DadosEpisodio(@JsonAlias("Title") String titulo, // o atributo "Title" da resposta da API será armazenado no atributo "titulo" classe
                             @JsonAlias("Episode") Integer numero,
                             @JsonAlias("imdbRating") String avaliacao,
                             @JsonAlias("Released") String dataLancamento) {
