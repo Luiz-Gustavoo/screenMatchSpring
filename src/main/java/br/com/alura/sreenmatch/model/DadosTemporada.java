6f12a9ca1ff9bb17583495661c79df0dd62ff2f6
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-21 21:09:21 -0300

    Adicionar comentários

--- src/main/java/br/com/alura/sreenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/sreenmatch/model/DadosTemporada.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episodes") List<DadosEpisodio> listaEpisodios) {
+                             @JsonAlias("Episodes") List<DadosEpisodio> listaEpisodios) { // salva o episódio na lista no padrão DadosEpsisodio
 }
