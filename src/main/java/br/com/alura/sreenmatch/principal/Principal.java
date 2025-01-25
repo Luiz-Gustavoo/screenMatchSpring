703b3b0593d42eab78e96eefda141cec1ea32b3a
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-24 21:23:15 -0300

    Adicionar classe ConsumirAPI e ConverteDados
    Adicionar consulta de série e converter a série em DadosSerie

--- src/main/java/br/com/alura/sreenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/sreenmatch/principal/Principal.java
@@ -1,16 +1,25 @@
 package br.com.alura.sreenmatch.principal;
 
 
+import br.com.alura.sreenmatch.model.DadosSerie;
+import br.com.alura.sreenmatch.service.ConsumirAPI;
+import br.com.alura.sreenmatch.service.ConverteDados;
+
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitor = new Scanner(System.in);
     private final String ENDERECO = "https://omdbapi.com/?t=";
     private final String API_KEY = "&apikey=7589c63";
+    private ConsumirAPI consumirAPI = new ConsumirAPI();
+    private ConverteDados converteDados = new ConverteDados();
 
     public void exibeMenu(){
         System.out.println("Digite o nome da série");
         String nomeSerie = leitor.nextLine();
 
+        String consultaSerie = consumirAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie serieConvertida = converteDados.converteDados(consultaSerie, DadosSerie.class);
+        System.out.println(serieConvertida);
     }
 }
