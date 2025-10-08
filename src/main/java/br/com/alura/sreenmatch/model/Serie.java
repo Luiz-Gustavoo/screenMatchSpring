81eb7686902e839111b641ea6bb87cc0c614de41
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-10-07 21:50:22 -0300

    Adicionar getters, setters e toString

--- src/main/java/br/com/alura/sreenmatch/model/Serie.java
+++ src/main/java/br/com/alura/sreenmatch/model/Serie.java
@@ -12,7 +12,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    Serie(DadosSerie dadosSerie) {
+    public Serie(DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
         this.Totaltemporadas = dadosSerie.Totaltemporadas();
         this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
@@ -20,5 +20,73 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotaltemporadas() {
+        return Totaltemporadas;
+    }
+
+    public void setTotaltemporadas(Integer totaltemporadas) {
+        Totaltemporadas = totaltemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "genero=" + genero +
+                ", titulo= " + titulo + '\'' +
+                ", Totaltemporadas=" + Totaltemporadas +
+                ", avaliacao=" + avaliacao +
+                ", atores='" + atores + '\'' +
+                ", poster='" + poster + '\'' +
+                ", sinopse='" + sinopse + '\'';
     }
 }
