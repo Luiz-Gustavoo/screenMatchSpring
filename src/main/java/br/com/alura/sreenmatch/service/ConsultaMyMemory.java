d466d77e1a5edadb8dafc34903dd433cfdfbbae1
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-10-11 17:32:07 -0300

    Alterar o método obterTraducao para estático

--- src/main/java/br/com/alura/sreenmatch/service/ConsultaMyMemory.java
+++ src/main/java/br/com/alura/sreenmatch/service/ConsultaMyMemory.java
@@ -3,23 +3,22 @@ package br.com.alura.sreenmatch.service;
 import java.net.URLEncoder;
 
 public class ConsultaMyMemory {
-    ConsumirAPI consumirAPI = new ConsumirAPI();
-    ConverteDados converteDados = new ConverteDados();
+    static ConsumirAPI consumirAPI = new ConsumirAPI();
+    static ConverteDados converteDados = new ConverteDados();
 
-    public String retornaDados(String texto){
+    public static String obterTraducao(String texto){
         try {
 
             String textoTraduzir = URLEncoder.encode(texto);
             String langpair = URLEncoder.encode("en|pt-br");
 
             String retornoApi = consumirAPI.obterDados("https://api.mymemory.translated.net/get?q="+textoTraduzir+"&langpair="+langpair);
-            System.out.println(retornoApi);
             DadosTraducao dadosTraducao = converteDados.converteDados(retornoApi, DadosTraducao.class);
-            System.out.println(dadosTraducao);
-            return retornoApi;
+
+            return dadosTraducao.dadosResposta().textoTraduzido();
         } catch (Exception e){
             System.out.println("Ocorreu um erro: " + e.getMessage());
         }
-        return "erro";
+        return null;
     }
 }
