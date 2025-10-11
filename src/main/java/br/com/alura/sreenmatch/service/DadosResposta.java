96955df2e453396cb7720130b25a2d9fdf026886
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-10-11 17:20:43 -0300

    Adicionar record DadosResposta

---
+++ src/main/java/br/com/alura/sreenmatch/service/DadosResposta.java
@@ -0,0 +1,8 @@
+package br.com.alura.sreenmatch.service;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosResposta(@JsonAlias("translatedText") String textoTraduzido) {
+}
